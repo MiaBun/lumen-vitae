093f642b421e854896edb7c33578fd50be55547c
Your Name <you@example.com>
2025-11-19 02:10:41 +0100

    latin names for saplings added

---
+++ modpack/overrides/kubejs/client_scripts/saplings.js
@@ -0,0 +1,13 @@
+ItemEvents.tooltip(event => {
+
+    event.add('minecraft:oak_sapling', Text.yellow('Quercus'))
+    event.add('minecraft:spruce_sapling', Text.yellow('Picea'))
+    event.add('minecraft:birch_sapling', Text.yellow('Betula'))
+    event.add('minecraft:jungle_sapling', Text.yellow('Ceiba'))
+    event.add('minecraft:acacia_sapling', Text.yellow('Acacia'))
+    event.add('minecraft:dark_oak_sapling', Text.yellow('Quercus Velutina'))
+    event.add('minecraft:cherry_sapling', Text.yellow('Prunus Cerasus'))
+    event.add('minecraft:crimson_fungus', Text.yellow('Crimson Fungus'))
+    event.add('minecraft:warped_fungus', Text.yellow('Warped Fungus'))
+    event.add('minecraft:mangrove_propagule', Text.yellow('Mangrovum'))
+})
